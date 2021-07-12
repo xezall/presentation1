11b3ecd45fec5f6f1dd5509d30467d143f88c6d2
xezall <ceylanxezalkaya@gmail.com>
2021-07-12 15:49:55 -0400

    hello

---
+++ src/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        /*
+        git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/xezall/presentation1.git
+git push -u origin main
+         */
+    }
+}
